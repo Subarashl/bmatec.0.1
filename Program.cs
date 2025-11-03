0bbe90a2eb404ad4a60c04479ace7a0ddb1003d3
Subarashl <Luizmoreirabranco@gmail.com>
2025-11-03 10:49:38 -0300

    Projeto bmatec

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "API da Bmatec conectada!");
+
+app.MapGet("/callback", (HttpRequest req) =>
+{
+    var code = req.Query["code"];
+    return $"Código recebido do Mercado Livre: {code}";
+});
+
+app.Run();
